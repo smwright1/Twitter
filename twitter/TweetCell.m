8a3132890ebbead17989f16d9ffbc7dfab31ea28
smwright1 <59848745+smwright1@users.noreply.github.com>
2021-06-30 17:30:02 -0400

    implemented (un)favoriting and (un)retweeting

--- twitter/TweetCell.m
+++ twitter/TweetCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "TweetCell.h"
+#import "APIManager.h"
 
 @implementation TweetCell
 
@@ -16,6 +17,77 @@
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
+}
+
+- (IBAction)didTapFavorite:(id)sender {
+    self.tweet.favorited = !self.tweet.favorited;
+    if (self.tweet.favorited) {
+        self.tweet.favoriteCount += 1;
+        [[APIManager shared] favorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+             if(error) {
+                  NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+             }
+             else {
+                 NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+             }
+         }];
+    }
+    else {
+        self.tweet.favoriteCount -= 1;
+        [[APIManager shared] unfavorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+             if(error) {
+                  NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+             }
+             else {
+                 NSLog(@"Successfully unfavorited the following Tweet: %@", tweet.text);
+             }
+         }];
+    }
+    [self refreshData];
+}
+
+- (IBAction)didTapRetweet:(id)sender {
+    self.tweet.retweeted = !self.tweet.retweeted;
+    if (self.tweet.retweeted) {
+        self.tweet.retweetCount += 1;
+        [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+             if(error) {
+                  NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+             }
+             else {
+                 NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+             }
+         }];
+    }
+    else {
+        self.tweet.retweetCount -= 1;
+        [[APIManager shared] unretweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+             if(error) {
+                  NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+             }
+             else {
+                 NSLog(@"Successfully unretweeted the following Tweet: %@", tweet.text);
+             }
+         }];
+    }
+    [self refreshData];
+}
+
+-(void)refreshData{
+    [self.retweetButton setTitle:[NSString stringWithFormat: @"%d", self.tweet.retweetCount] forState:UIControlStateNormal];
+    if (self.tweet.retweeted) {
+        [self.retweetButton setImage: [UIImage imageNamed:@"retweet-icon-green"] forState:UIControlStateNormal];
+    }
+    else {
+        [self.retweetButton setImage: [UIImage imageNamed:@"retweet-icon"] forState:UIControlStateNormal];
+    }
+    [self.favoriteButton setTitle:[NSString stringWithFormat: @"%d", self.tweet.favoriteCount] forState:UIControlStateNormal];
+    if (self.tweet.favorited) {
+        [self.favoriteButton setImage: [UIImage imageNamed:@"favor-icon-red"] forState:UIControlStateNormal];
+    }
+    else {
+        [self.favoriteButton setImage: [UIImage imageNamed:@"favor-icon"] forState:UIControlStateNormal];
+    }
 }
 
 @end
